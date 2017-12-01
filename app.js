c580288f0276aaa172193a9051e4599e9577dd32
Nidhi16 <nidhi.valuable@gmail.com>
2017-12-01 23:21:04 +0530

    Use event module and experimented on it

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
 var http = require('http');
+// Load event module
 var events = require('events');
 
+// Create an event emitter object
 var eventEmitter = new events.EventEmitter();
 
 var server = http.createServer(function(request, response) {
-    response.writeHead('200', {'Content-Type':200});
-    var connectionHandler = function() {
-        console.log("Connection Succesful");
-        response.write("Connection Succesful");
-        
-    };
-    
-    eventEmitter.on('connection', connectionHandler);
-    
-    eventEmitter.emit('connection');
+    // Fire the connection event
+    eventEmitter.emit('connection', response);
+});
+
+// Bind the myevent event with the anonymous function
+eventEmitter.on('myevent', function(response, data){
+    console.log(data);
+    response.write("\n");
+    response.write(data);
     response.end();
 });
+
+
+// Create an event handler as follows
+var connectionHandler = function(response) {
+    console.log("Connection Succesful"); 
+    response.write("Connection Succesful");
+    // Fire the custom event with data 
+    eventEmitter.emit('myevent', response, "This is test event");
+};
+
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectionHandler);
 
 server.listen(8000);
